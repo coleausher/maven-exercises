66eaef4f393dd2c8cb575d4498642dd8e77b35c2
Cole Usher <coleausher97@gmail.com>
2023-05-04 14:27:26 -0500

    Maven Exercise complete

---
+++ src/main/java/MavenExercises.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+//
+//Create a new class with a main method that prompts the user for input and does the following (using the StringUtils class)
+//
+//        Tells whether or not what the user entered is a number
+//        Flips the case of the string
+//        Reverses the string
+//        Example Console Output
+//
+//
+//        Enter Something: Life, The Universe, and Everything
+//        You Entered: "Life, The Universe, and Everything"
+//        "Life, The Universe, and Everything" is not a number
+//        Flipped Case: lIFE, tHE uNIVERSE, AND eVERYTHING
+//        Reversed: gnihtyrevE dna ,esrevinU eht ,efiL
+
+public class MavenExercises {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Something: ");
+        String input = scanner.nextLine();
+        System.out.println("You Entered: " + input);
+        System.out.println("Flipped Case: " + input.toUpperCase());
+        System.out.println("Reversed String: " + new StringBuilder(input).reverse().toString());
+        System.out.println("Origional String: " + input);
+
+
+
+
+    }
+}
